e984143f75d209bad85e5d0bdeccb6584f5de441
ricardosobrado <ricardosobrado@gmail.com>
2018-09-16 10:59:04 +0200

    Initial commit

---
+++ tutorials-datinApp.Api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using tutorials_datinApp.Api.Models;
+
+namespace tutorials_datinApp.Api.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ tutorials-datinApp.Api/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace tutorials_datinApp.Api.Models
+{
+    public class Value
+    {
+        public int Id {get; set;}
+
+        public string Name { get; set; }
+    }
+}
